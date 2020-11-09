028ffa2f64a9f7480d88947a9dfd6f68265df7c6
Lucas Tidas <Lucas@pc-225.home>
2020-11-09 13:40:49 +0100

    initial

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios";
 
-function App() {
+const App = () => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchData = async () => {
+    const response = await axios.get("http://localhost:3001/");
+    setData(response.data);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
-    <div>
+    <div className="container">
       Hello from <a href="https://www.lereacteur.io">Le Reacteur !</a>
     </div>
   );
-}
+};
 
 export default App;
